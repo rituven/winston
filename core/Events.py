2d1963e06f05e27fdbf64a657667668f5bb11c51
Ashwin Venkatesan <ashwinjv@gmail.com>
2016-09-04 17:41:14 -0700

    Added Events enum and listener class

--- core/Events.py
+++ core/Events.py
@@ -1,4 +1,11 @@
-class Event(object):
+from enum import Enum
+class Events(Enum):
     """
-    An example event that can be raised
+    Events Enum
     """
+    UI_BTN_PRESSED = 100
+    UI_BTN_RELEASED = 101
+    UI_BTN_CLICKED = 102
+
+    SET_UI_BTN_STATE = 150
+    
